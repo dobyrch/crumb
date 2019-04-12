d1fe749dbc050cf6cd24c836550ab533f486d1be
Douglas Christman <DouglasChristman@gmail.com>
2019-04-12 23:05:00 +0800

    New crumbd using latest and greatest fanotify features

--- crumbd.c
+++ crumbd.c
@@ -1,220 +1,281 @@
 #define _GNU_SOURCE
+#include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <limits.h>
 #include <poll.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/fanotify.h>
+#include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/fanotify.h>
+#include <sys/inotify.h>
 #include <unistd.h>
-#include <sqlite3.h>
 
-#define DELETED " (deleted)"
 #define FAN_CREATE             0x00000100      /* Subfile was created */
 #define FAN_MARK_FILESYSTEM    0x00000100
 #define FAN_REPORT_FID         0x00000200      /* Report unique file id */
+#define FAN_REPORT_FILENAME    0x00000400      /* Report file name */
 
-static void fan_allow(int fd, int mfd)
-{
-	struct fanotify_response response;
+#define FAN_EVENT_INFO_TYPE_FID		1
 
-	/* Allow file to be opened */
-	response.fd = mfd;
-	response.response = FAN_ALLOW;
-	write(fd, &response, sizeof(struct fanotify_response));
-}
+/* Variable length info record following event metadata */
+struct fanotify_event_info_header {
+	__u8 info_type;
+	__u8 pad;
+	__u16 len;
+};
 
-static void store_event(int fd, sqlite3 *db)
-{
-	const struct fanotify_event_metadata *m;
-	struct fanotify_event_metadata buf[4096];
-	ssize_t len;
-	char path1[PATH_MAX], path2[PATH_MAX], prog[PATH_MAX];
-	ssize_t path_len1, path_len2;
-	char procfd_path[PATH_MAX];
-	char procexe_path[PATH_MAX];
-
-	for (;;) {
-
-		len = read(fd, (void *) &buf, sizeof(buf));
-		if (len == -1 && errno != EAGAIN) {
-			perror("read");
-			exit(EXIT_FAILURE);
-		}
-
-		if (len <= 0)
-			break;
-
-		for (m = buf; FAN_EVENT_OK(m, len); m = FAN_EVENT_NEXT(m, len)) {
-
-			if (m->vers != FANOTIFY_METADATA_VERSION) {
-				fprintf(stderr, "Mismatch of fanotify metadata version.\n");
-				exit(EXIT_FAILURE);
-			}
-
-			if (m->fd < 0) {
-				fan_allow(fd, m->fd);
-				continue;
-			}
-
-			snprintf(procfd_path, sizeof(procfd_path),
-					"/proc/self/fd/%d", m->fd);
-			path_len1 = readlink(procfd_path, path1,
-					sizeof(path1) - 1);
-			if (path_len1 < 0) {
-				perror("readlink");
-				exit(EXIT_FAILURE);
-			}
-
-			path1[path_len1] = '\0';
-			if (path_len2 != -1) {
-				path2[path_len2] = '\0';
-			}
-
-			snprintf(procexe_path, sizeof(procexe_path),
-					"/proc/%d/exe", m->pid);
-			path_len2 = readlink(procexe_path, path2,
-					sizeof(path2) - 1);
-
-			// TODO: don't skip if deleted, just remove it
-			// if empty after removal, log error and continue
-			// (after permitting access, that is)
-			if (strstr(path1, DELETED) || strstr(path2, DELETED) ||
-					path1[0] == '\0' || path2[0] == '\0') {
-				fan_allow(fd, m->fd);
-				continue;
-			}
-
-
-			struct stat st;
-			int err;
-
-			err = fstat(m->fd, &st);
-			fan_allow(fd, m->fd);
-
-			if (err != 0) {
-				perror("fstat");
-				continue;
-			}
-
-			if (st.st_size != 0) {
-				continue;
-			}
-
-			fprintf(stderr, "[%s] %s\n", path2, path1);
-
-			// This doesn't work
-			/*
-			if (access(path1, F_OK) == 0) {
-				fprintf(stderr, "%s already exists\n", path1);
-			} else {
-				fprintf(stderr, "%s does NOT exist\n", path1);
-			}
-			*/
-
-			close(m->fd);
-
-			sqlite3_stmt *statement = NULL;
-			const char *tail;
-
-			int r;
-			r = sqlite3_prepare_v2(db,
-				"insert or ignore into "
-				"paths (path, exe) "
-				"values (?, ?) ",
-				100, // nicer way of reliably getting length
-				&statement,
-				&tail);
-			//fprintf(stderr, "TAIL: %s\n", tail);
-			//fprintf(stderr, "prepare: %d\n", r);
-
-
-
-			r = sqlite3_bind_text(statement, 1, path1, path_len1, SQLITE_STATIC);
-			//fprintf(stderr, "bind1: %d\n", r);
-			r = sqlite3_bind_text(statement, 2, path2, path_len2, SQLITE_STATIC);
-			//fprintf(stderr, "bind2: %d\n", r);
-
-			r = sqlite3_step(statement);
-			//fprintf(stderr, "step: %d\n", r);
-			//fprintf(stderr, sqlite3_errmsg(db));
-			// TODO: sqlite3_clear_bindings (maybe reset?), and finalize at end
-			r = sqlite3_finalize(statement);
-			//fprintf(stderr, "finalize: %d\n", r);
-			//fprintf(stderr, "\n");
-			//r = sqlite3_close(db);
-			//fprintf(stderr, "close: %d\n", r);
-		}
-	}
-}
-
-int main(void)
-{
-	struct pollfd *pfd;
-	sqlite3 *db;
-	int r, fd;
-
-	/* TODO: create db during installation so it gets cleaned up
-	 * create table if not exists paths
-	 * (path, exe, primary key (path, exe))
-	 * without rowid;
+/* Unique file identifier info record */
+struct fanotify_event_info_fid {
+	struct fanotify_event_info_header hdr;
+	__kernel_fsid_t fsid;
+	/*
+	 * Following is an opaque struct file_handle that can be passed as
+	 * an argument to open_by_handle_at(2).
 	 */
-	r = sqlite3_open("/var/lib/crumb/crumb.db", &db);
-	if (r != SQLITE_OK) {
-		fprintf(stderr, "sqlite3_open: %s\n", sqlite3_errmsg(db));
+	unsigned char handle[0];
+};
+
+#define BUF_SIZE 4096
+
+void process_fanotify(int);
+void process_inotify(int);
+
+int main(int argc, char **argv)
+{
+	int fd, fdi, ret;
+	struct pollfd fds[2];
+
+	fdi = inotify_init();
+	ret = inotify_add_watch(fdi, "/home/dobyrch/.mozilla/firefox/joc9538c.default-1521425002138", IN_CREATE);
+	if (ret == -1) {
+		perror("inotify_add_watch");
 		exit(EXIT_FAILURE);
 	}
 
-	fd = fanotify_init(FAN_REPORT_FID, 0);
-	if (fd < 0) {
+
+	/* Create an fanotify file descriptor with FAN_REPORT_FID as a flag
+	 * so that program can receive fid events.
+	 */
+	fd = fanotify_init(FAN_CLASS_NOTIF | FAN_REPORT_FID | FAN_REPORT_FILENAME, 0);
+	if (fd == -1) {
 		perror("fanotify_init");
 		exit(EXIT_FAILURE);
 	}
 
-	r = fanotify_mark(fd, FAN_MARK_ADD | FAN_MARK_FILESYSTEM , FAN_ONDIR|FAN_CREATE , -1, "/home/dobyrch");
-	if (r < 0) {
-		perror("fanotify_mark home");
-		exit(EXIT_FAILURE);
-	}
-
-/*
-	r = fanotify_mark(fd, FAN_MARK_ADD | FAN_MARK_ONLYDIR, FAN_OPEN_PERM | FAN_ONDIR | FAN_EVENT_ON_CHILD, -1, "/home/dobyrch/.config");
-	if (r < 0) {
+	/* Place a mark on the filesystem object supplied in argv[1]. */
+	ret = fanotify_mark(fd, FAN_MARK_ADD | FAN_MARK_FILESYSTEM,
+						FAN_CREATE | FAN_ONDIR,
+						AT_FDCWD, "/home/dobyrch/");
+	if (ret == -1) {
 		perror("fanotify_mark");
 		exit(EXIT_FAILURE);
 	}
 
-	r = fanotify_mark(fd, FAN_MARK_ADD | FAN_MARK_ONLYDIR, FAN_OPEN_PERM | FAN_ONDIR | FAN_EVENT_ON_CHILD, -1, "/home/dobyrch/.local/share");
-	if (r < 0) {
-		perror("fanotify_mark");
-		exit(EXIT_FAILURE);
-	}
+	printf("Listening for events.\n");
 
-	r = fanotify_mark(fd, FAN_MARK_ADD | FAN_MARK_ONLYDIR, FAN_OPEN_PERM | FAN_ONDIR | FAN_EVENT_ON_CHILD, -1, "/home/dobyrch/.cache");
-	if (r < 0) {
-		perror("fanotify_mark");
-		exit(EXIT_FAILURE);
-	}
-	*/
-
-	pfd->fd = fd;
-	pfd->events = POLLIN;
-
-	fprintf(stderr, "Listening for events.\n");
+	fds[0].fd = fd;
+	fds[1].fd = fdi;
+	fds[0].events = POLLIN;
+	fds[1].events = POLLIN;
 
 	for (;;) {
-		r = poll(pfd, 1, -1);
-		if (r < 0) {
-			if (errno == EINTR)
-				continue;
-
+		ret = poll(fds, 2, -1);
+		if (ret == -1) {
 			perror("poll");
 			exit(EXIT_FAILURE);
 		}
 
-		 if (pfd->revents & POLLIN)
-			store_event(fd, db);
+		if (fds[0].revents & POLLIN) {
+			process_fanotify(fds[0].fd);
+		} else if (fds[1].revents & POLLIN) {
+			process_inotify(fds[1].fd);
+		} else {
+			assert(false);
+		}
+	}
+}
+
+void process_inotify(int fd) {
+	char buf[4096] __attribute__ ((aligned(__alignof__(struct inotify_event))));
+	const struct inotify_event *event;
+	int i;
+	ssize_t len;
+	char *ptr;
+
+	/* Read some events. */
+
+	len = read(fd, buf, sizeof buf);
+	if (len == -1 && errno != EAGAIN) {
+		perror("read");
+		exit(EXIT_FAILURE);
+	}
+
+	/* If the nonblocking read() found no events to read, then
+	   it returns -1 with errno set to EAGAIN. In that case,
+	   we exit the loop. */
+
+	if (len <= 0)
+		return;
+
+	/* Loop over all events in the buffer */
+
+	for (ptr = buf; ptr < buf + len;
+			ptr += sizeof(struct inotify_event) + event->len) {
+
+		event = (const struct inotify_event *) ptr;
+
+		if ((event->mask & IN_CREATE) == 0)
+			return;
+
+		/* Print the name of the file */
+
+		if (event->len)
+			printf("I: %s ", event->name);
+
+		/* Print type of filesystem object */
+
+		if (event->mask & IN_ISDIR)
+			printf("[directory]\n");
+		else
+			printf("[file]\n");
+	}
+}
+
+void process_fanotify(int fd) {
+	int ret;
+	ssize_t len, path_len, exe_path_len;
+	char *filename;
+	char path[PATH_MAX];
+	char exe_path[PATH_MAX];
+	char procfd_path[PATH_MAX];
+	char procexe_path[PATH_MAX];
+	char events_buf[BUF_SIZE];
+
+	struct file_handle *file_handle;
+	struct fanotify_event_metadata *metadata;
+	struct fanotify_event_info_fid *fid;
+
+	/* Read events from the event queue into a buffer */
+	len = read(fd, (void *) &events_buf, sizeof(events_buf));
+	if (len == -1 && errno != EAGAIN) {
+		perror("read");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Process all events within the buffer */
+	for (metadata = (struct fanotify_event_metadata *) events_buf;
+			FAN_EVENT_OK(metadata, len);
+			metadata = FAN_EVENT_NEXT(metadata, len)) {
+
+
+
+		/* Print the exe that created the new file/dir */
+		snprintf(procexe_path, sizeof(procexe_path),
+				"/proc/%d/exe", metadata->pid);
+		exe_path_len = readlink(procexe_path, exe_path,
+				sizeof(exe_path) - 1);
+		if (exe_path_len != -1) {
+			exe_path[exe_path_len] = '\0';
+		}
+
+		printf("Creator: %s (pid %d)\n", exe_path, metadata->pid);
+
+
+
+		fid = (struct fanotify_event_info_fid *) (metadata + 1);
+		file_handle = (struct file_handle *) fid->handle;
+		filename = (char *) (file_handle + 1);
+		// What is this extra stuff?
+		filename += 8;
+
+		/*
+		printf("Filename: %s\n", filename);
+		printf("ptrdiff: %ld\n", filename - (char *)metadata);
+		printf("event_len: %u\n", metadata->event_len);
+		printf("sizeof metadata: %ld\n", sizeof(struct fanotify_event_metadata));
+		printf("sizeof fid: %ld\n", sizeof(struct fanotify_event_info_fid));
+		printf("sizeof handle: %ld\n", sizeof(struct file_handle));
+		*/
+
+		/* Ensure that the event info is of the correct type */
+		if (fid->hdr.info_type != FAN_EVENT_INFO_TYPE_FID) {
+			fprintf(stderr, "Received unexpected event type.\n");
+			exit(EXIT_FAILURE);
+		}
+
+		/*
+		printf("====START\n");
+		for (int x = 0; x < metadata->event_len; ++x) {
+			unsigned char cc = ((char *)metadata)[x];
+
+			if (cc < 33)
+				printf("0x%02X\n", cc);
+			else
+				printf("%c\n", cc);
+		}
+		printf("=====STOP\n");
+		*/
+
+		/* metadata->fd is set to FAN_NOFD when FAN_REPORT_FID is enabled.
+		 * To obtain a file descriptor for the file object corresponding to
+		 * an event you can use the struct file_handle that's provided
+		 * within the fanotify_event_info_fid in conjunction with the
+		 * open_by_handle_at(2) system call. A check for -ESTALE is done
+		 * to accommodate for the situation where the file handle was
+		 * deleted for the object prior to this system call.
+		 */
+		//printf("metadata->fd: %d\n", metadata->fd);
+		fd = open_by_handle_at(AT_FDCWD, file_handle, O_RDONLY);
+		if (ret == -1 && errno == ESTALE) {
+			printf("File handle is no longer valid. File has been deleted\n");
+			continue;
+		} else if (ret == -1) {
+			perror("open_by_handle_at");
+			exit(EXIT_FAILURE);
+		}
+
+		snprintf(procfd_path, sizeof(procfd_path), "/proc/self/fd/%d", fd);
+
+		/* Retreive and print the path of the modified dentry */
+		path_len = readlink(procfd_path, path, sizeof(path) - 1);
+		if (path_len == -1) {
+			perror("readlink");
+			exit(EXIT_FAILURE);
+		}
+
+		path[path_len] = '/';
+		/* TODO: check bounds */
+		strcpy(&path[path_len+1], filename);
+		/*
+		printf("F: %s ", path);
+		*/
+
+		/*
+		if (metadata->mask == FAN_CREATE)
+			printf("[file]\n");
+
+		if (metadata->mask == (FAN_CREATE | FAN_ONDIR))
+			printf("[directory]\n");
+		*/
+
+		/* Close associated file decriptor for this event */
+		close(fd);
+
+		/*
+		printf("==START\n");
+		for (char *c = path; *c != '\0'; ++c) {
+			if (*c)
+				putchar(*c);
+			else
+				putchar('*');
+		}
+		printf("\n===STOP\n");
+		*/
+
+		printf("Created: %s\n\n", path);
 	}
 }
