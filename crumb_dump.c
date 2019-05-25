b143ec11b6776f9b1bd18652ed1b9b3e212854d8
Douglas Christman <DouglasChristman@gmail.com>
2019-05-26 02:07:05 +0800

    Use ftw to traverse directories

--- crumb_dump.c
+++ crumb_dump.c
@@ -1,112 +1,42 @@
-#include <dirent.h>
+#define _XOPEN_SOURCE 500
 #include <errno.h>
-#include <fcntl.h>
+#include <ftw.h>
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 #include <sys/stat.h>
-#include <sys/types.h>
+#include <sys/resource.h>
 #include <sys/xattr.h>
 
 static int status = EXIT_SUCCESS;
-static int recursive = 0, alloc_depth = 64, max_depth;
-static char **path_list;
+static int recursive = 0, max_depth;
 
-void dumpxattr(int dir_fd, char *file_name, int path_depth)
+int printxattr(const char *path, const struct stat *stat, int type, struct FTW *ftw)
 {
-	int file_fd, ret, i;
-	DIR *nextdir;
-	struct dirent *entry;
-	struct stat statbuf;
 	ssize_t attr_len;
 	char attr[PATH_MAX];
 
-	/* NONBLOCK needed to prevent hanging when opening named pipes */
-	file_fd = openat(dir_fd, file_name, O_RDONLY | O_NONBLOCK | O_NOFOLLOW);
 
-	if (file_fd == -1) {
-		/* For now ignore ELOOP (symbolic links) and ENXIO (sockets)
-		   once the daemon supports setting attrs on symlinks, then
-		   call open with O_PATH|O_NOFOLLOW to get an fd of a symlink */
-		if (errno != ELOOP && errno != ENXIO) {
-			fprintf(stderr, "%s: %s\n", file_name, strerror(errno));
-			status = EXIT_FAILURE;
-		}
-
-		return;
-	}
-
-
-	attr_len = fgetxattr(file_fd, "user.crumb-exe", attr, sizeof(attr));
+	attr_len = getxattr(path, "user.crumb-exe", attr, sizeof(attr));
 
 	if (attr_len  >= 0) {
-		for (i = 0; i < path_depth; ++i) {
-			printf("%s/", path_list[i]);
-		}
-
-		printf("%s%c%.*s%c", file_name, '\0', (int)attr_len, attr, '\0');
+		printf("%s%c%.*s%c", path, '\0', (int)attr_len, attr, '\0');
 	} else if (errno != ENODATA) {
 		/* TODO: swap perror for null-terminated fprintf */
 		perror("fgetxattr");
+		fprintf(stderr, "%s\n", path);
 		status = EXIT_FAILURE;
 	}
 
+	/* TODO: Stop when max depth exceeded */
 
-	if (recursive && path_depth < max_depth) {
-		ret = fstat(file_fd, &statbuf);
-
-		if (ret == -1) {
-			perror("stat");
-			status = EXIT_FAILURE;
-			goto close_file;
-		}
-
-		if (!S_ISDIR(statbuf.st_mode)) {
-			goto close_file;
-		}
-
-		nextdir = fdopendir(file_fd);
-
-		if (nextdir == NULL) {
-			perror("fdopendir");
-			status = EXIT_FAILURE;
-			goto close_file;
-		}
-
-		path_list[path_depth++] = file_name;
-
-		if (path_depth >= alloc_depth) {
-			alloc_depth *= 2;
-			path_list = realloc(path_list, alloc_depth * sizeof(char *));
-
-			if (path_list == NULL) {
-				perror("realloc");
-				status = EXIT_FAILURE;
-				goto close_file;
-			}
-		}
-		
-		while ((entry = readdir(nextdir)) != NULL) {
-			if (strcmp(entry->d_name, ".") == 0
-			    || strcmp(entry->d_name, "..") == 0) {
-				continue;
-			}
-
-			dumpxattr(dirfd(nextdir), entry->d_name, path_depth);
-		}
-
-		closedir(nextdir);
-	}
-
-close_file:
-	close(file_fd);
+	return 0;
 }
 
 int main(int argc, char **argv)
 {
 	int opt, i;
+	struct rlimit rlimit;
 
 	while ((opt = getopt(argc, argv, "r::")) != -1) {
 		switch (opt) {
@@ -120,18 +50,14 @@ int main(int argc, char **argv)
 		}
 	}
 
-	path_list = malloc(alloc_depth * sizeof(char *));
-
-	if (path_list == NULL) {
-		perror("malloc");
-		return EXIT_FAILURE;
+	if (getrlimit(RLIMIT_NOFILE, &rlimit) == -1) {
+		perror("getrlimit");
 	}
 
 	for (i = optind; i < argc; ++i) {
-		dumpxattr(AT_FDCWD, argv[i], 0);
+		/* TODO: WHY IS IT TRYING TO FOLLOW LINKS???? */
+		nftw(argv[i], printxattr, rlimit.rlim_cur, FTW_PHYS);
 	}
-
-	free(path_list);
 
 	return status;
 }
