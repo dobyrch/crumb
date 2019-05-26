838e6b97960634fd6ce54a6edd9986a3fe01ac34
Douglas Christman <DouglasChristman@gmail.com>
2019-05-26 21:41:03 +0800

    Use error() for error handling

--- crumb_dump.c
+++ crumb_dump.c
@@ -1,34 +1,34 @@
+#define _GNU_SOURCE
 #define _XOPEN_SOURCE 500
 #include <errno.h>
+#include <error.h>
 #include <ftw.h>
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/stat.h>
 #include <sys/resource.h>
 #include <sys/xattr.h>
 
-static int status = EXIT_SUCCESS;
-static int recursive = 0, max_depth;
+static long max_depth = 0;
 
 int printxattr(const char *path, const struct stat *stat, int type, struct FTW *ftw)
 {
 	ssize_t attr_len;
 	char attr[PATH_MAX];
 
+	if (ftw->level > max_depth) {
+		return FTW_SKIP_SIBLINGS;
+	}
 
 	attr_len = lgetxattr(path, "user.crumb-exe", attr, sizeof(attr));
 
 	if (attr_len  >= 0) {
-		printf("%s%c%.*s%c", path, '\0', (int)attr_len, attr, '\0');
+		printf("%s%c%.*s%c", path, '\0', (int) attr_len, attr, '\0');
 	} else if (errno != ENODATA) {
-		/* TODO: swap perror for null-terminated fprintf */
-		perror("lgetxattr");
-		fprintf(stderr, "%s\n", path);
-		status = EXIT_FAILURE;
+		error(0, errno, "%s", path);
 	}
-
-	/* TODO: Stop when max depth exceeded */
 
 	return 0;
 }
@@ -36,13 +36,18 @@ int printxattr(const char *path, const struct stat *stat, int type, struct FTW *
 int main(int argc, char **argv)
 {
 	int opt, i;
+	char *endptr;
 	struct rlimit rlimit;
 
 	while ((opt = getopt(argc, argv, "r::")) != -1) {
 		switch (opt) {
 		case 'r':
-			recursive = 1;
-			max_depth = optarg ? atoi(optarg) : INT_MAX;
+			max_depth = optarg ? strtol(optarg, &endptr, 10) : LONG_MAX;
+
+			if (optarg && *endptr != '\0') {
+				error(EXIT_FAILURE, 0, "invalid depth '%s'", optarg);
+			}
+
 			break;
 		case '?':
 			/* getopt prints an error message to stdout */
@@ -51,12 +56,12 @@ int main(int argc, char **argv)
 	}
 
 	if (getrlimit(RLIMIT_NOFILE, &rlimit) == -1) {
-		perror("getrlimit");
+		error(EXIT_FAILURE, errno, "getrlimit");
 	}
 
 	for (i = optind; i < argc; ++i) {
-		nftw(argv[i], printxattr, rlimit.rlim_cur - 3, FTW_PHYS);
+		nftw(argv[i], printxattr, rlimit.rlim_cur / 2, FTW_PHYS | FTW_ACTIONRETVAL);
 	}
 
-	return status;
+	return error_message_count > 0 ? EXIT_FAILURE : EXIT_SUCCESS;
 }
